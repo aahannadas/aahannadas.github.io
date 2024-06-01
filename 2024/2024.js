9c19de9cbfe0060b47ae8e9f56fa767a4c241b7a
Neil Dembla <neildembla1@gmail.com>
2024-06-01 16:20:06 -0400

    added default html to all /2024/*.html pages, changed css on index.html a bit, added .gitignore, added phantom sans font

---
+++ 2024/2024.js
@@ -0,0 +1,15 @@
+fetch("https://api.github.com/repos/aahannadas/aahannadas.github.io/git/trees/main?recursive=1")
+	.then(res => res.json())
+	.then(json => {
+		text = json["tree"]
+		for (i = 0; i < text.length; i++) {
+			file = text[i]
+			path = file.path;
+
+			if (path.startsWith("2024/") && path.includes(".html")) {
+				file = path.replace("2024/", "").replace(".html", "")
+				if (file == "2024" || file == ".sample.html") continue
+				document.getElementById("websites").innerHTML += `<a class="display-5 text-light" href="/2024/${file}">${file}'s page</a><br/>`;
+			}
+		}
+	});
