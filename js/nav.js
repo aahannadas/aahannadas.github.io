fd258c9ab22d03161cec43973ae2194d705a6b34
Neil <neildembla1@gmail.com>
2024-06-02 03:11:34 -0400

    added content to neil's page. removed bg-dark class from navbar, making it transparent (hopefully)

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ fetch("/assets/nav.html")
 	.then(text => {
 		let oldElem = document.querySelector("script#replace_with_navbar");
 		let newElem = document.createElement("nav");
-		newElem.className = "navbar navbar-expand-lg bg-dark";
+		newElem.className = "navbar navbar-expand-lg"; // bg-dark
 		newElem.innerHTML = text;
 		oldElem.parentNode.replaceChild(newElem, oldElem);
 	});
